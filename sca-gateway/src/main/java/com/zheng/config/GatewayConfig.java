f3080ea8d44a34f78ab7a7b96ecb2aeac7bbf568
zhengchaoz <34977102+zhengchaoz@users.noreply.github.com>
2021-06-07 17:18:11 +0800

    zipkin链路追踪 郑超 2021年6月7日17:17:49

--- sca-gateway/src/main/java/com/zheng/config/GatewayConfig.java
+++ sca-gateway/src/main/java/com/zheng/config/GatewayConfig.java
@@ -1,11 +1,9 @@
 package com.zheng.config;
 
-import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
 import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
 import com.alibaba.fastjson.JSON;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
